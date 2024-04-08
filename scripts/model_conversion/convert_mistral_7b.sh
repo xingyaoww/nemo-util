b1f8dfdf2351f13fb35a3eb463fec88102a117c7
Xingyao Wang <xingyao6@illinois.edu>
2024-04-08 09:53:05 -0500

    fix megatron version issue

--- scripts/model_conversion/convert_mistral_7b.sh
+++ scripts/model_conversion/convert_mistral_7b.sh
@@ -3,7 +3,11 @@ echo "You supposed to run this script inside the docker container"
 
 MODEL_PATH=/models/Mistral-7B-v0.1/
 OUTPUT_NAME=mistral-7b-base.nemo
+
+# Override the installed version of Megatron-LM, but use the cloned version
+export PYTHONPATH=$(pwd)/NeMo:$(pwd)/Megatron-LM:$PYTHONPATH
+
 python3 NeMo/scripts/checkpoint_converters/convert_mistral_7b_hf_to_nemo.py \
     --input_name_or_path=$MODEL_PATH \
     --output_path=data/models/nemo/$OUTPUT_NAME \
-    --precision=bf16
+    --precision=bf16-mixed
